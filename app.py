bffdc7aac437ee1687720d7f6b3e79efd2b271fd
Alfred <42361881+Xyloidzzz@users.noreply.github.com>
2022-04-29 22:59:51 -0500

    debug OFF

--- app.py
+++ app.py
@@ -920,4 +920,4 @@ def display_correlation_info(correlation_ticker1, correlation_ticker2):
     return "Correlation: " + str(format(corr[0], '.6f')) + "... " + "P-value: " + str(format(corr[1], '.6f'))
 
 
-app.run_server(debug=True)
+app.run_server()  # debug=True
